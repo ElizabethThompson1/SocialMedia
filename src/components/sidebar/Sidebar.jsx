9b243aef558721156b9f64dc555c2d28574bace0
Stanley Pethel <stanleypethel@yahoo.com>
2021-12-05 22:28:25 -0500

    finalized style and icon appearence

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,36 @@
 import "./sidebar.css"
+import {RssFeed, People, Comment, PersonAdd} from "@material-ui/icons"
 
 export default function Sidebar(){
     return(
         <div className="sidebar">
-                sidebar
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    <li className= "sidebarListItem">
+                     <RssFeed className="sidebarIcon" />
+                     <span className="sidebarListItemText">Feed</span>
+                    </li>
+                    <li className= "sidebarListItem">
+                     <People className="sidebarIcon" />
+                     <span className="sidebarListItemText">Friends</span>
+                    </li>
+                    <li className= "sidebarListItem">
+                     <Comment className="sidebarIcon" />
+                     <span className="sidebarListItemText">Comments</span>
+                    </li>
+                    <li className= "sidebarListItem">
+                     <PersonAdd className="sidebarIcon" />
+                     <span className="sidebarListItemText">FriendRequests</span>
+                    </li>
+                </ul>
+                <button className="sidebarButton">Show More</button>
+                <hr className="sidebarHr"/>
+                <ul className="sidebarFriendList">
+                    <li className="sidebarFriend">
+                        <img src="" alt=""/>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
