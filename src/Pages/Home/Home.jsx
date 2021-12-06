45378062d8bc3e716e18cd294ba93c253cd10f22
Stanley Pethel <stanleypethel@yahoo.com>
2021-12-05 21:30:57 -0500

    corrected css files

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import TopBar from "../../components/Topbar/topBar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./home.css"
 
 export default function Home(){
     return(
      <>
         <TopBar />
+        <div className="homecontainer">
         <Sidebar/>
+        <Feed/>
+        <Rightbar/>
+        </div>
      </>
     );
 }
