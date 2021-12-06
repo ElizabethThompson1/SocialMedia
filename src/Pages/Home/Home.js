b295aec7f3c3cc0bd436b37588cd24723c5cd1b4
Stanley Pethel <stanleypethel@yahoo.com>
2021-12-05 21:16:10 -0500

    updated new files

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,11 @@
 import TopBar from "../../components/Topbar/topBar";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 export default function Home(){
     return(
+     <>
         <TopBar />
-    )
+        <Sidebar/>
+     </>
+    );
 }
