20500cbfcf9076597d98afbcfc5422694d644780
unknown <drew17509@gmail.com>
2021-12-06 10:03:23 -0700

    add files

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,7 @@
 import "./sidebar.css"
 import {RssFeed, People, Comment, PersonAdd} from "@material-ui/icons"
+import {Link} from "react-router-dom";
+
 
 export default function Sidebar(){
     return(
