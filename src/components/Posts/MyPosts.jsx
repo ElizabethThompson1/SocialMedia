635452c33d7f5e89ba552ca6faa31fe6799098b2
unknown <drew17509@gmail.com>
2021-12-07 10:00:24 -0700

    edited post

--- src/components/Posts/MyPosts.jsx
+++ src/components/Posts/MyPosts.jsx
@@ -4,46 +4,56 @@ import {useState} from "react";
 import {Users} from "../../dummyData";
 
 export default function post({ post }){
-    const[like,setLike] = useState(post.like)
-    const [isLiked,setIsLiked] = useState(false)
-    const PF = process.env.REACT_APP_PUBLIC_FOLDER
+    const[like,setLike] = useState(post.like.lenght);
+    const [isLiked,setIsLiked] = useState(false);
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
-    const likeHandler =()=>{
-        setLike(isLiked ? like-1 : like+1)
-        setIsLiked(!isLiked)
-}
+
+    userEffect(() => {
+        const fetchUser = async () => {
+            const res = await axios.get(`users/ ${post.userId} `);
+            setUser(res.data)
+        };
+        fetchUser();
+    }, [post.userId] );
+
+    const likeHandler = () => {
+        setLike(isLiked ? like-1 : like+1);
+        setIsLiked(!isLiked);
+};
 
 }
 return (
          <div className="post">
-                           <div className="postWrapper">
-                           <div className= "postTop">
-                              <div className= "postTopLeft">
-                              <img
+            <div className="postWrapper">
+                 <div className= "postTop">
+                     <div className= "postTopLeft">
+                         <img
                           className="postProfileimg" 
-                                  src={User.filter((u) => u.id === post?.userId)[0].profilePicture}
-                                alt=""
-                                 />
-                                     <span className="postUsername">
-                                        {User.filter((u) => u.id === post?.userId) [0].username}</span>
-                                        </div>
-                                      <div className="postTopRight">
-                                                    <MoreVert/>
-                                                    </div>
-                                                    </div>
-                                                <div className="postCenter">
-                                                    <span className="postText"></span>
-                                                    <img className="postImg" src={PF+post.photo} alt=""/>
-                                                    </div>
-                                                    <div className="postBottom">
-                                                        <div className="postBottomLeft">
-                                                            <img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt=""/>
-                                    <img className="postLikeCounter">{like} people like it</img>
-                                    </div><div className="postBottomRight">
-                                        </div> 
+                         src={User.profilePicture || PF + "person/noAvatar.png"}
+                             alt=""
+                             />
+                             <span className="postUsername">
+                             {User.filter((u) => u.id === post?.userId) [0].username}</span>
+                                  </div>
+                                 <div className="postTopRight">
+                                   <MoreVert/>
+                                     </div>
+                                     </div>
+                                     <div className="postCenter">
+                                      <span className="postText">{post?.desc} </span>
+                                          <img className="postImg" src={PF+post.img} alt=""/>
+                                             </div>
+                                             <div className="postBottom">
+                                                 <div className="postBottomLeft">
+                                                 <img className="likeIcon" src={`${PF}like.png`} onClick={likeHandler} alt=""/>
 
-                                    </div>
-                                                </div>
-                </div>
+                                                  <img className="postLikeCounter">{like} people like it</img>
+                                                  </div><div className="postBottomRight">
+                                                 </div> 
+
+                                                  </div>
+                                                   </div>
+                                                      </div>
 
 )
